cd0cefa5564232c67a15963cd48f178d1355128d
Zaharije Pasalic <zaharije@klika.ba>
2018-06-04 14:57:52 +0200

    Shared schemas

--- SwiftTemplate/AppDelegate.swift
+++ SwiftTemplate/AppDelegate.swift
@@ -18,15 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
         MSAppCenter.start(AppConfiguration.sharedInstance().appCenterId,
                           withServices:[ MSAnalytics.self, MSCrashes.self ])
-
         return true
     }
-
-
 
     func applicationWillResignActive(_ application: UIApplication) {
     }
